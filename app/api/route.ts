febb97cb5ef3a7dd121b7720e8daf6690d092ff2
Alex Yang <himself65@outlook.com>
2025-11-19 17:56:44 -0800

    feat: add api

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,5 @@
-export async function GET (req: any) {
+export async function GET (req: never) {
   // log req url
-  console.log(req.url)
+  console.log(req)
   return Response.json({ status: 'ok' })
 }
